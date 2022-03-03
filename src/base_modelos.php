14b9c9d814afa4710845efdb4354ba33556af3d4
mgamboa <gamboa.martin@gmail.com>
2022-03-03 17:20:20 -0600

    v 1.1.0

--- src/base_modelos.php
+++ src/base_modelos.php
@@ -1,6 +1,7 @@
 <?php
 namespace gamboamartin\base_modelos;
 
+use gamboamartin\errores\errores;
 use gamboamartin\validacion\validacion;
 
 class base_modelos extends validacion
@@ -32,34 +33,52 @@ class base_modelos extends validacion
     }
 
     /**
-     *
+     * Funcion para validar si una vivienda puede ser o no entregada
      * @param array $cliente
      * @return array|bool
      */
     public function valida_entrega_cliente(array $cliente): array|bool
     {
-
+        $keys = array('estado_entrega_entregada','estado_entrega_inicial');
+        $valida = $this->valida_statuses($cliente,$keys);
+        if(errores::$error){
+            return $this->error->error('Error al validar cliente', $valida);
+        }
         if ($cliente['estado_entrega_entregada'] === 'activo') {
             return $this->error->error('Error la vivienda ha sido entregada', $cliente);
         }
         if ($cliente['estado_entrega_inicial'] === 'activo') {
             return $this->error->error(
-                'Error la vivienda no puede ser entregada porque cliente[estado_entrega_inicial] es activo', $cliente);
+                'Error la vivienda no puede ser entregada porque cliente[estado_entrega_inicial] es activo',
+                $cliente);
         }
         return true;
     }
 
 
+    /**
+     * Válida si una operacion en un registro está inactiva en su campo status data error
+     * @param bool $aplica_transaccion_inactivo
+     * @param int $registro_id
+     * @param string $tabla
+     * @param array $registro
+     * @return array|bool
+     */
     public function valida_transaccion_activa(bool  $aplica_transaccion_inactivo, int $registro_id, string $tabla,
                                               array $registro): array|bool
     {
+        $tabla = trim($tabla);
+        if($tabla === ''){
+            return $this->error->error('Error la tabla esta vacia', $tabla);
+        }
         if (!$aplica_transaccion_inactivo) {
             if ($registro_id <= 0) {
                 return $this->error->error('Error el id debe ser mayor a 0', $registro_id);
             }
             $key = $tabla . '_status';
             if (!isset($registro[$key])) {
-                return $this->error->error('Error no existe el registro con el campo ' . $tabla . '_status', $registro);
+                return $this->error->error('Error no existe el registro con el campo ' . $tabla . '_status',
+                    $registro);
             }
             if ($registro[$tabla . '_status'] === 'inactivo') {
                 return $this->error->error('Error el registro no puede ser manipulado', $registro);
--- tests/src/base_modelosTest.php
+++ tests/src/base_modelosTest.php
@@ -59,6 +59,109 @@ class base_modelosTest extends test {
         errores::$error = false;
     }
 
+    public function test_valida_entrega_cliente(): void
+    {
+        errores::$error = false;
+        $base_modelo = new base_modelos();
+        $cliente = array();
+        $resultado = $base_modelo->valida_entrega_cliente($cliente);
+        $this->assertIsArray( $resultado);
+        $this->assertStringContainsStringIgnoringCase('Error al validar cliente', $resultado['mensaje']);
+        $this->assertTrue(errores::$error);
+        errores::$error = false;
+
+        $cliente = array();
+        $cliente['estado_entrega_entregada'] = 'activo';
+        $resultado = $base_modelo->valida_entrega_cliente($cliente);
+        $this->assertIsArray( $resultado);
+        $this->assertStringContainsStringIgnoringCase('Error al validar cliente', $resultado['mensaje']);
+        $this->assertTrue(errores::$error);
+        errores::$error = false;
+
+        $cliente = array();
+        $cliente['estado_entrega_entregada'] = 'activo';
+        $cliente['estado_entrega_inicial'] = 'activo';
+        $resultado = $base_modelo->valida_entrega_cliente($cliente);
+        $this->assertIsArray( $resultado);
+        $this->assertStringContainsStringIgnoringCase('Error la vivienda ha sido entregada', $resultado['mensaje']);
+        $this->assertTrue(errores::$error);
+
+        errores::$error = false;
+
+        $cliente = array();
+        $cliente['estado_entrega_entregada'] = 'inactivo';
+        $cliente['estado_entrega_inicial'] = 'activo';
+        $resultado = $base_modelo->valida_entrega_cliente($cliente);
+        $this->assertIsArray( $resultado);
+        $this->assertStringContainsStringIgnoringCase('Error la vivienda no puede ser entregada', $resultado['mensaje']);
+        $this->assertTrue(errores::$error);
+
+        errores::$error = false;
+
+        $cliente = array();
+        $cliente['estado_entrega_entregada'] = 'inactivo';
+        $cliente['estado_entrega_inicial'] = 'inactivo';
+        $resultado = $base_modelo->valida_entrega_cliente($cliente);
+        $this->assertIsBool( $resultado);
+        $this->assertTrue($resultado);
+        $this->assertNotTrue(errores::$error);
+
+    }
+
+    public function test_valida_transaccion_activa(): void
+    {
+        errores::$error = false;
+        $base_modelo = new base_modelos();
+        $aplica_transaccion_inactivo = false;
+        $registro_id = -1;
+        $tabla = '';
+        $registro = array();
+        $resultado = $base_modelo->valida_transaccion_activa($aplica_transaccion_inactivo, $registro_id,
+            $tabla, $registro);
+        $this->assertIsArray( $resultado);
+        $this->assertStringContainsStringIgnoringCase('Error la tabla esta vacia', $resultado['mensaje']);
+        $this->assertTrue(errores::$error);
+
+        errores::$error = false;
+
+        $aplica_transaccion_inactivo = false;
+        $registro_id = -1;
+        $tabla = 'a';
+        $registro = array();
+        $resultado = $base_modelo->valida_transaccion_activa($aplica_transaccion_inactivo, $registro_id,
+            $tabla, $registro);
+        $this->assertIsArray( $resultado);
+        $this->assertStringContainsStringIgnoringCase('Error el id debe ser mayor a 0', $resultado['mensaje']);
+        $this->assertTrue(errores::$error);
+
+        errores::$error = false;
+
+        $aplica_transaccion_inactivo = false;
+        $registro_id = 1;
+        $tabla = 'a';
+        $registro = array();
+        $resultado = $base_modelo->valida_transaccion_activa($aplica_transaccion_inactivo, $registro_id,
+            $tabla, $registro);
+        $this->assertIsArray( $resultado);
+        $this->assertStringContainsStringIgnoringCase('Error no existe el registro con el campo a_status', $resultado['mensaje']);
+        $this->assertTrue(errores::$error);
+
+        errores::$error = false;
+
+        $aplica_transaccion_inactivo = false;
+        $registro_id = 1;
+        $tabla = 'a';
+        $registro = array();
+        $registro['a_status'] = '';
+        $resultado = $base_modelo->valida_transaccion_activa($aplica_transaccion_inactivo, $registro_id,
+            $tabla, $registro);
+
+        $this->assertIsBool( $resultado);
+        $this->assertTrue($resultado);
+        $this->assertNotTrue(errores::$error);
+
+    }
+
 
 
 
